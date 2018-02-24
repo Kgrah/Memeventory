a25fdc29a38b6c752404a30634255a031c85c005
kgrah <kylelgrah@gmail.com>
2018-02-23 22:18:43 -0800

    unfuck typescript, add sidebar template

--- memeventory/src/components/SideBar.vue
+++ memeventory/src/components/SideBar.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="hello">
-    <table>
-      <tr>Hey there</tr>
-    </table>
+  <div class="sidenav">
+    <div class="side-nav">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -10,20 +10,41 @@
 import { Vue, Component } from 'vue-property-decorator'
 
 @Component
-export default class SideBar extends Vue {
+class SideBar extends Vue {
 
 }
+
+export default SideBar;
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-tr {
-  width: 100 px;
-  height: 30px;
+.side-nav {
+    height: 100%;
+    width: 200px;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color:white;
+    overflow-x: hidden;
+    transition: 0.5s;
+    padding-top: 60px;
+    box-shadow: 1.5px 1.5px 2px 1.5px #ccc;
 }
-
-table {
-  border: 2px solid;
-  box-shadow: lightgrey;
+img {
+  float: left;
+}
+a {
+    display: inline;
+    font-family: 'Roboto', sans-serif;
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    font-size: 25px;
+    color: grey;
+    transition: 0.3s;
+    text-align: left;
+    box-shadow: 0px 0px 0px 0px #ccc;
+    float: left;
 }
 </style>
---
+++ memeventory/src/components/SideBarItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="sidebaritem">
+      <img src="https://www.shareicon.net/data/32x32/2017/04/28/885302_wheel_512x512.png"/>
+      <div>
+          <!-- will become <router-link></router-link>-->
+          <a href="www.reddit.com"></a>
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class SideBarItem extends Vue {
+
+}
+</script>
+
+<style>
+
+</style>
