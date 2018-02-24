0a79409999f844482eb0fe704755d21105cfbabc
kgrah <kylelgrah@gmail.com>
2018-02-23 20:46:21 -0800

    need more vue

---
+++ memeventory/src/components/SideBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <table>
+      <tr>Hey there</tr>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class SideBar extends Vue {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+tr {
+  width: 100 px;
+  height: 30px;
+}
+
+table {
+  border: 2px solid;
+  box-shadow: lightgrey;
+}
+</style>
